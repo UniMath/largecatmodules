4daa2562df89e2274faf4d8c9bf6a5af342460fb
amblaf <tamere@tonpere.com>
2016-11-29 16:07:31 +0100

    configuration emacs

---
+++ .dir-locals.el
@@ -0,0 +1,21 @@
+((coq-mode
+  . ((eval . 
+	     (let ((unimath-topdir (concat (expand-file-name (locate-dominating-file buffer-file-name "coq")) "coq/UniMath/")))
+	     (setq fill-column 100)
+	     (make-local-variable 'coq-use-project-file)
+	     (setq coq-use-project-file nil)
+	     (make-local-variable 'coq-prog-args)
+	     (setq coq-prog-args `("-emacs" "-indices-matter" "-type-in-type" "-Q" "." "Largecat" "-R" ,(concat unimath-topdir "UniMath") "UniMath" "-R" "TypeTheory/TypeTheory" "TypeTheory" ))
+	     (make-local-variable 'coq-prog-name)
+	     (setq coq-prog-name "coqtop")
+	     (make-local-variable 'before-save-hook)
+	     (add-hook 'before-save-hook 'delete-trailing-whitespace)
+	     (modify-syntax-entry ?' "w")
+	     (modify-syntax-entry ?_ "w")
+	     (if (not (memq 'agda-input features))
+		 (load (concat unimath-topdir "emacs/agda/agda-input")))
+	     (if (not (member '("chimney" "╝") agda-input-user-translations))
+		 (progn
+		   (setq agda-input-user-translations (cons '("chimney" "╝") agda-input-user-translations))
+		   (agda-input-setup)))
+	     (set-input-method "Agda"))))))
