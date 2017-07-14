d0ad77bd8223eef6b8f570e0e17a4c3655f5c7e4
amblaf <you@example.com>
2017-07-14 18:47:23 +0200

    fichier à part quotient de représentation

---
+++ Modules/Prelims/quotientrep.v
@@ -0,0 +1,342 @@
+
+Require Import UniMath.Foundations.PartD.
+
+Require Import UniMath.CategoryTheory.Monads.
+Require Import UniMath.CategoryTheory.LModules.
+Require Import UniMath.CategoryTheory.SetValuedFunctors.
+Require Import UniMath.CategoryTheory.HorizontalComposition.
+Require Import UniMath.CategoryTheory.functor_categories.
+Require Import UniMath.CategoryTheory.categories.category_hset.
+Require Import UniMath.CategoryTheory.categories.category_hset_structures.
+
+Require Import UniMath.CategoryTheory.Categories.
+
+Require Import UniMath.Foundations.Sets.
+Require Import UniMath.CategoryTheory.Epis.
+
+Require Import UniMath.CategoryTheory.EpiFacts.
+
+Require Import Modules.Prelims.lib.
+Require Import Modules.Prelims.quotientmonad.
+Require Import UniMath.CategoryTheory.whiskering.
+
+
+Local Notation "α ∙∙ β" := (horcomp β α) (at level 20).
+Local Notation "'SET'" := hset_category.
+Local Notation "F ;;; G" := (nat_trans_comp _ _ _ F G) (at level 35).
+
+Open Scope cat.
+
+(* Trouvé dans SubstitutionsSystem/Notation *)
+Notation "α 'ø' Z" := (pre_whisker Z α)  (at level 25).
+Notation "Z ∘ α" := (post_whisker α Z) (at level 50, left associativity).
+
+
+
+
+
+
+    
+Set Automatic Introduction.
+
+Section QuotientMonad.
+
+  Context {R : Monad SET} {eqrel_equivc : ∏ c, eqrel (( R c):hSet)}
+          (congr_equivc : ∏ (x y:SET) (f:SET⟦x,y⟧),
+                          iscomprelrelfun (eqrel_equivc x) (eqrel_equivc y) (# R f))
+          (compat_μ_projR: (compat_μ_projR_def congr_equivc)).
+
+Let equivc {c:hSet} x y := eqrel_equivc c x y.
+  
+(* We define the quotient functor of R by these equivalence relations
+
+The following comment may be outdated
+----
+I need to make it not convertible to quot_functor otherwise
+Coq takes a long time to check or compute things.
+For example :   R' ((R' □ R') c) = (R' □ R') (R' c) by reflexivity
+is so slow when R' is definitely equal to quot_functor !
+----
+
+*)
+  (* FIN DE LA SECONDE ETAPE *)
+
+Let R' := R' congr_equivc.
+Let projR := projR congr_equivc.
+Let R'_monad  := R'_monad congr_equivc compat_μ_projR.
+Let projR_monad  := projR_monad congr_equivc compat_μ_projR.
+
+Local Notation π := projR_monad.
+  Local Notation Θ := tautological_LModule.
+
+
+  (*
+Let a be a R -module
+    b      R'-module
+
+Let τ  : a -> R a R-module morphism
+    h  : a -> π^*(b) a R-module morphism (where π := projR)
+(this could certainly be generalized to the case where the  codomain of τ is an
+arbitrary R-module but we are interested in the category of representations)
+
+We want to define a R'-module morphism τ' : b -> R' defined by the following diagram :
+
+<<<<<<<<<<<
+
+        τ
+   a ------> R
+   |         |
+   |         |
+ h |         | π
+   |         |
+   V         V
+   b ------> R'
+       τ'
+
+>>>>>>>>>>>>
+
+so we need that h is epimorphic (uniqueness) and that
+h is compatible with π ∘ τ
+*)
+Context {a : LModule R SET} {b: LModule R'_monad SET}
+          {τ : LModule_Mor _ a (Θ R)}
+          {h:LModule_Mor _ a (pb_LModule projR_monad b)}
+          (* TODO : define the general type of compatability that is used everywhere to define
+            a morphism out of an epimorphism (Cf quotient monad : compat_μ_def ..) *)
+          (compath:∏ X (x y:(a X:hSet)), h X x = h X y -> 
+                                         (τ X · projR_monad X) x = (τ X · projR_monad X) y)
+          (* TODO : demander que ce soit pointwise epi plutôt *)
+          (isEpih : isEpi (C:=[SET,SET]) (h:nat_trans _ _)).
+Definition R'_rep_τ  : nat_trans b R'.
+Proof.
+  (* TODO : adapter univ_surj_nt pour que ça demande epi pointwise pour h *)
+  apply (univ_surj_nt _ (((τ:nat_trans _ _):[SET,SET]⟦_,_⟧) · (projR_monad:nat_trans _ _)) compath).
+  apply isEpih.
+Defined.
+
+Local Notation τ' := R'_rep_τ.
+           
+Definition R'_rep_τ_def :
+  ∏ X, h X · τ' X = τ X · projR_monad X.
+Proof.
+  apply ((univ_surj_nt_ax_pw _ (((τ:nat_trans _ _):[SET,SET]⟦_,_⟧) · (projR_monad:nat_trans _ _)) compath) isEpih).
+Qed.
+
+
+  Local Notation σ := (lm_mult _).
+(**
+
+ τ' is a module morphism
+
+This is the following diagram :
+
+<<<<<<<<<<<
+
+          τ' R'
+   b R' --------> R' R'
+   |               |
+   |               |
+σ b|               | μ'
+   |               |
+   V               V
+   b ------------> R'
+           τ'
+
+>>>>>>>>>>>>
+
+To show this, we post-compose with h·π (horizontal composition)
+(see t'_law_eq1 & 2)
+
+
+ 
+
+ *)
+
+Local Notation "α 'ø' Z" := (pre_whisker Z α)  (at level 25).
+Local Notation "Z ∘ α" := (post_whisker α Z) (at level 50, left associativity).
+(**
+h·π ;; τ' R' ;; μ' = (h ;; τ')·π ;; μ'   ( property of horizontal comp)
+                   = (τ ;; π )·π ;; μ'   (definition of τ')
+                   = (τ·id) ;; π ·π ;; μ' (propoerty of horizontal comp)
+                   = τ R    ;; μ ;; π     (π is a monad morphism)
+                   = σ a    ;; τ  ;; π     (τ is a module morphism)
+*)
+Lemma τ'_law_eq1 X : ((h∙∙projR_monad :[SET,SET]⟦_,_⟧)
+                     · (τ' ø R'(* ∘ τ' *):nat_trans _ (functor_composite R' R'))
+                     · (μ R'_monad):nat_trans _ _) X =
+                     ((σ a:[SET,SET]⟦_,_⟧) · (τ:nat_trans _ _) · projR : nat_trans _ _) X.
+Proof.
+  etrans.
+  apply cancel_postcomposition.
+  etrans.
+  etrans;[eapply pathsinv0; apply assoc|].
+  apply cancel_precomposition.
+  apply (nat_trans_ax τ').
+  etrans; [apply assoc|].
+  apply cancel_postcomposition.
+  (* definition of τ' *)
+  apply R'_rep_τ_def.
+  repeat rewrite <- assoc.
+  etrans.
+  apply cancel_precomposition.
+  (* π est un morphisme de moande (par définition) *)
+  apply R'_μ_def.
+  rewrite assoc.
+  etrans.
+  apply cancel_postcomposition.
+  apply (LModule_Mor_σ R τ). 
+  reflexivity.
+Qed.
+
+
+(*
+And on the other side
+
+
+h·π ;; σ b ;; τ' = h R ;; R' π ;; σ b ;; τ' (property of horizontal comp)
+                 = h R ;; σ (π^* b) ;; τ'   (definition of a pull back module)
+                 = σ a ;; h ;; τ'   (h is a module morphism)
+                 = σ a ;; τ ;; π    (definition of τ')
+*)
+Lemma τ'_law_eq2 X : ((h∙∙projR_monad :[SET,SET]⟦_,_⟧)
+                     · (σ b)
+                     · τ':nat_trans _ _) X =
+                     ((σ a:[SET,SET]⟦_,_⟧) · (τ:nat_trans _ _) · projR :nat_trans _ _) X.
+Proof.
+  etrans.
+  apply cancel_postcomposition.
+  apply (LModule_Mor_σ R h). 
+  repeat rewrite <- assoc.
+  apply cancel_precomposition.
+  apply R'_rep_τ_def.
+Qed.
+
+Lemma R'_rep_τ_module_laws : LModule_Mor_laws _ (T:=b)
+                                           (T':=  tautological_LModule R'_monad)
+                                           R'_rep_τ.
+Proof.
+  intro X.
+  
+  (* En vrai, je n'ai pas besoin ici que ce soit un epi pointwise (me semble-t-il)*)
+  assert (epi : isEpi (* (C:=functor_Precategory SET SET) *)
+                  ((  ( 
+                                                      h )    ∙∙ projR) X)).
+  {
+    apply Pushouts_pw_epi.
+    apply PushoutsHSET_from_Colims.
+    apply isEpi_horcomp.
+    apply isEpi_projR.
+    intro Y.
+    apply Pushouts_pw_epi.
+    apply PushoutsHSET_from_Colims.
+    apply isEpih.
+  }
+  apply epi.
+  etrans.
+  apply τ'_law_eq1.
+  apply pathsinv0.
+  apply τ'_law_eq2.
+Qed.    
+
+Definition R'_rep_τ_module :LModule_Mor _ b
+                                     ( tautological_LModule R'_monad) :=
+  (_ ,, R'_rep_τ_module_laws).
+
+(** Let S a monad, m : R -> S a monad morphism compatible with the equivalence relation
+This induces a monad morphism u : R' -> S that makes the following diagram commute
+(Cf quotientmonad)
+
+<<<<<<<<<<<<<
+     m
+  R --> S
+  |     ^
+  |    /
+π |   / u
+  |  /
+  V /
+  R'
+
+>>>>>>>>>>>>
+
+
+*)
+Context {S:Monad SET}  {m: Monad_Mor R S}
+  (compatm: ∏ (X:SET) (x y : (R X:hSet)), projR X x = projR X y → m X x = m X y).
+
+Let u_monad := quotientmonad.u_monad compat_μ_projR _ compatm.
+
+(**
+Let c be a S-Module
+    F : b -> u^*(c) a module morphism.
+    s: c -> S a S-module morphism
+
+We also suppose that the following diagram commute (m should be a representation
+morphism)
+
+<<<<<<<<<<<
+
+               τ
+        a  ----------> R
+        |              |
+        |              |
+h ;; F  |              | m
+        |              |
+        V              V
+        c -----------> S
+                s
+
+>>>>>>>>>>>>
+
+Then the following diagram commute :
+
+<<<<<<<<<<<
+
+               τ'
+        b    --------> R'
+        |              |
+        |              |
+     F  |              | u
+        |              |
+        V              V
+        c -----------> S
+                s
+
+>>>>>>>>>>>>
+
+Let us prove this by post-composition by the h : a -> b
+
+h ;; τ' ;; u =  τ ;; π ;; u (definition of τ')
+             =  τ ;; m      (definition of u)
+             =  h ;; F ;; s      (previous commuting diagram for m)
+
+*)
+Context {c:LModule S SET} {s: LModule_Mor _ c (Θ S)}
+          {F:LModule_Mor _ b (pb_LModule u_monad c)}.
+
+Variable (compat_m_rep : ∏ X, τ X · m X = h X · F X · s X).
+
+
+(* j'ai besoin que h est pointwise un epi pour pouvoir montrer cette version pw*)
+Lemma u_rep_laws X : F X · s X = τ' X · u_monad X.
+Proof.
+  assert (epih_pw : forall X, isEpi (h X)).
+  {
+    apply Pushouts_pw_epi.
+    apply PushoutsHSET_from_Colims.
+    apply isEpih.
+  }
+  apply epih_pw.
+  apply pathsinv0.
+  etrans.
+  rewrite assoc.
+  apply cancel_postcomposition.
+  apply R'_rep_τ_def.
+  etrans.
+  rewrite <- assoc.
+  apply cancel_precomposition.
+  eapply pathsinv0.
+  apply quotientmonad.u_def.
+  now apply compat_m_rep.
+Qed.
+
+End QuotientMonad.
--- Modules/Prelims/uniproof.v
+++ Modules/Prelims/uniproof.v
@@ -79,6 +79,7 @@ Require Import Modules.Prelims.arities.
 Require Import Modules.Prelims.lib.
 Require Import Modules.Prelims.modules.
 Require Import Modules.Prelims.quotientmonad.
+Require Import Modules.Prelims.quotientrep.
 
 
 
@@ -186,7 +187,7 @@ Section CandidatU.
 Context {S:REP b} (m:R -->[ F] S).
 
 Lemma compat_m :  ∏ (X : SET) (x y : ((pr1 R) X:hSet)),
-  (quotientmonad.projR congr_equivc) X x = (quotientmonad.projR congr_equivc) X y
+  projR X x = projR X y
   → (pr1 m) X x = (pr1 m) X y.
 Proof.
   intros X x y eqpr;
@@ -388,13 +389,44 @@ End eq_mr.
 
 Open Scope arity_scope.
 
+(**
+
+<<<<
+
+       hab
+   a_R --> b_R'
+    |     ^
+    |    /
+a_π |   / F_R'
+    |  /
+    V /
+    a_R'
+
+>>>>
+where π := projR
+
+ *)
+Definition hab : mor_disp
+                   (D:=bmod_disp_ob_mor _ _ )(a` (pr1 R)) (b`  R'_monad)
+                   ((projR_monad:category_Monad _⟦_,_⟧) ;; identity _).
+  set (aπ :=(# a projR_monad )%ar).
+  eapply (comp_disp 
+                    (D:=bmod_disp _ _ )
+                    (yy:=a`R'_monad)
+         ).
+  apply (#a projR_monad)%ar.
+  apply (F`` R'_monad).
+Defined.
+
 (** This is the compatibility relation that is needed to construct
 R'_rep_τ : b R -> R
 *)
 Lemma compat_rep_τ_projR :
       ∏ (X : SET) x y,
-      ( pr1 (# a projR_monad )%ar X ;;  (F `` R'_monad) X) x
-      =       ( pr1 (# a projR_monad )%ar X ;; (F `` R'_monad) X) y
+       (pr1 hab) X x
+      (* =       ( pr1 (# a projR_monad )%ar X ;; (F `` R'_monad) X) y *)
+       =
+       pr1 hab  X y
       (* (( armor_ob _ F (pr1 R) X ) ;; pr1 (# b projR_monad )%ar X) x *)
       (* = (( armor_ob _ F (pr1 R) X ) ;; pr1 (# b projR_monad )%ar X) y *)
       ->
@@ -420,7 +452,6 @@ Proof.
   apply comp.
 Qed.
 
-    
 
 (* F preserve the epis *)
 Definition isEpi_FR' := isEpi (C:=functor_precategory HSET HSET has_homsets_HSET)
@@ -433,205 +464,50 @@ Definition a_preserves_epi := ∏ M N (f:category_Monad _⟦M,N⟧),
 Context (Fepi:isEpi_FR') (aepi:a_preserves_epi).
 
 
-Lemma isEpi_def_R'_rep_τ : isEpi
-                          (compose (C:=functor_category _ _)
-                                   (pr1 ((# a)%ar projR_monad))
-                                   (pr1 (F `` R'_monad)%ar)).
+Lemma isEpi_def_R'_rep_τ : isEpi (C:= [SET,SET]) (pr1 hab).
+                          (* (compose (C:=functor_category _ _) *)
+                          (*          (pr1 ((# a)%ar projR_monad)) *)
+                          (*          (pr1 (F `` R'_monad)%ar)). *)
 Proof.
-  apply (isEpi_comp (functor_category _ _));[|apply Fepi].
+  apply (isEpi_comp (functor_category _ _)).
   apply aepi;    apply isEpi_projR_monad.
+  apply (isEpi_comp (functor_category _ _)).
+  apply Fepi.
+  (* TODO : améliorer cette preuve pourrie *)
+  intros u v w e.
+  cbn in e.
+  apply nat_trans_eq.
+  apply homset_property.
+  intro X.
+  eapply nat_trans_eq_pointwise in e.
+  cbn in e.
+  apply e.
 Qed.
 
+
+
+(*
 Definition R'_rep_τ  : nat_trans (pr1 ( b` R'_monad)) R'.
 Proof.
-  apply (univ_surj_nt (* (A:= ##R □ ##R) (B:=functor_composite R' R')                     *)
-           
-           ( pr1 (# a projR_monad )%ar ;;;(( F`` (R'_monad)  )   ))
-           (rep_τ _ R  ;;; projR)).
-  (* asbtract these *)
-  -  apply compat_rep_τ_projR.      
-  - apply isEpi_def_R'_rep_τ.
+eapply (quotientrep.R'_rep_τ congr_equivc compat_μ_projR (h:=hab)compat_rep_τ_projR isEpi_def_R'_rep_τ).
 Defined.
 
-Definition R'_rep_τ_def :
-  ∏ (X:SET),
-  ( (# a (projR_monad))%ar) X ;; (F`` R'_monad) X;;R'_rep_τ X  
-  =  rep_τ _ R X ;; projR X .
-Proof.
-  intro x.
-  abstract(
-      apply (univ_surj_nt_ax_pw ((pr1 (# a (projR_monad))%ar)  ;;; (F ``  R'_monad) ))).
-Qed.
+*)
 
 
-Lemma R'_rep_τ_module_laws : LModule_Mor_laws _ (T:=pr1 (b ` R'_monad))
-                                           (T':=  tautological_LModule R'_monad)
-                                           R'_rep_τ.
-Proof.
-  intro X.
-  
-  (* En vrai, je n'ai pas besoin ici que ce soit un epi pointwise (me semble-t-il)*)
-  assert (epi : isEpi (* (C:=functor_Precategory SET SET) *)
-                  ((  ( (pr1 (# a (projR_monad))%ar)  ;;;
-                                                      (F ``  R'_monad) )    ∙∙ projR) X)).
-  {
-    apply Pushouts_pw_epi.
-    apply PushoutsHSET_from_Colims.
-    apply isEpi_horcomp.
-    apply isEpi_projR.
-    intro Y.
-    apply Pushouts_pw_epi.
-    apply PushoutsHSET_from_Colims.
-    apply isEpi_def_R'_rep_τ.
-  }
-  apply epi.
-  cbn -[R' compose].
-  
-  (* Etape 1 : utiliser R'_rep_τ_def pour faire disparaître R'_rep_τ *)
-  etrans.    
-  rewrite (assoc (C:=SET)).
-  
-  apply (cancel_postcomposition (C:=SET)).
-
-  etrans.
-  apply (cancel_postcomposition (C:=SET)).
-  
-  (* je dois faire reculer le b avec la naturalité de F R' *)
-  etrans.
-  rewrite <- (assoc (C:=SET)).
-  apply (cancel_precomposition SET).
-  symmetry.
-  apply (nat_trans_ax (F`` R'_monad)).
-  (* je dois faire avance le #a projR_monad en avec la naturalité de a *)
-  rewrite  (assoc (C:=SET)).
-  apply (cancel_postcomposition (C:=SET)).
-  
-  symmetry.
-  apply (nat_trans_ax (pr1 ((# a)%ar projR_monad))).
-  
-  (* suppression de R'_rep_τ annoncé *)
-  rewrite <- (assoc (C:=SET)).
-  rewrite <- (assoc (C:=SET)).
-  apply (cancel_precomposition SET).
-  rewrite assoc.
-  apply R'_rep_τ_def.
-  
-    (* maintenant je dois supprimer R'_mu avec R'_μ_def
-  Je dois donc faire appraitre horcomp projR projR devant *)
-
-    (*
-      a_R p       m R'        p R'       mu'
-a_R R ---> a_R R' ----> R R' ---> R' R' ---> R'
-
-     *)
-    
-
-  etrans.
-  rewrite assoc.
-  (* 
-C'est bon : il faut intervertir a_R p et m R' et ça devrait être bon
-   *)
-  apply (cancel_postcomposition (C:=SET)).
-  apply (cancel_postcomposition (C:=SET)).
-  apply nat_trans_ax.
-  
-  etrans.
-  rewrite <- assoc.
-  rewrite <- assoc.
-  apply (cancel_precomposition SET).
-  rewrite assoc.
-  apply R'_μ_def.
-  
-  apply pathsinv0.
-  (* Dans l'autre sens maintenant *)
-  etrans.
-  (* 
-       a_p R         F_R' R       b_R' p         σ_b_R'      m'
-a_R R -----> a_R' R -----> b_R' R ----> b_R' R' -----> b_R' -----> R'
-   *)
-  (* But : utiliser R'_rep_τ_def pour faire disparaître R'_rep_τ = m' 
-
-Il faut utiliser le fait que F R' et a p sont des morphismes de modules.
-
-faire reculer b_R' p
-- par la naturalité de F_R'
-- par la naturalité de a_R
-
-   *)
-  
-  apply (cancel_postcomposition (C:=SET)).
-  etrans.
-  rewrite <- (assoc (C:=SET)).
-  apply (cancel_precomposition SET).
-  symmetry.
-  apply (nat_trans_ax ((F`` R'_monad))).
-  
-  rewrite assoc.
-  apply cancel_postcomposition.
-  symmetry.
-  apply (nat_trans_ax (pr1 ((# a)%ar projR_monad))).
-  
-  (* Maintenant on utilie le fait que F R' et p sont des morphismes de module *)
-  etrans.
-  rewrite assoc.
-  apply (cancel_postcomposition (C:=SET)).
-  repeat rewrite <- assoc.
-  etrans.
-  
-  apply (cancel_precomposition SET).
-  apply (cancel_precomposition SET).
-  
-  assert (hb := LModule_Mor_σ _ (  ( pr1 F R'_monad (tt))) X).
-  (* On se fait emmerder à cause de # b identitye = identite *)
-  assert (hid := functor_id (pr1 (pr1 b R'_monad (tt))) (R' X)).
-  use (pathscomp0 _ hb).
-  apply cancel_precomposition.
-  cbn.
-  apply funextfun.
-  apply toforallpaths in hid.
-  intro x.
-  cbn.
-  now rewrite hid.
-    
-
-    (* Ouf ! passons à #a p morphisme de module *)
-    rewrite assoc,assoc.
-    apply cancel_postcomposition.
-    (* ici negro *)
-
-    assert (ha := LModule_Mor_σ _ (  ( ((# a)%ar projR_monad))) ( X)).
-    use (pathscomp0 _ ha).
-    (* un petit de natural transformation_ax s'impose pour mettre projR_monad en premier *)
-    etrans.
-    apply cancel_postcomposition.
-    apply (nat_trans_ax (pr1 ((# a)%ar projR_monad))).
-    
-    apply funextfun.
-    intro x.
-    apply idpath.
-
-    (* voilà le travail ! maintenant on peu éliminer R'_rep_τ avec R'_rep_τ_def *)
-    etrans.
-    do 2  rewrite <- assoc.
-    apply (cancel_precomposition SET).
-    rewrite assoc.
-    apply R'_rep_τ_def.
-    rewrite assoc.    
-    rewrite (assoc (C:=SET)).    
-    apply (cancel_postcomposition (C:=SET)).
-    etrans.
-    (* m_R est un morphism de module *)
-
-    assert (hm := LModule_Mor_σ _ (  ( (rep_τ  SET R )) ) X).
-    symmetry.
-    apply hm.
-    apply idpath.
-Qed.    
 
 Definition R'_rep_τ_module :LModule_Mor _ (b` R'_monad)
                                      ( tautological_LModule R'_monad) :=
-  (_ ,, R'_rep_τ_module_laws).
+  (quotientrep.R'_rep_τ_module congr_equivc compat_μ_projR (h:=hab)compat_rep_τ_projR isEpi_def_R'_rep_τ).
 
+Definition R'_rep_τ_def :
+  ∏ (X:SET),
+  ( (# a (projR_monad))%ar) X ;; (F`` R'_monad) X;;R'_rep_τ_module X  
+  =  rep_τ _ R X ;; projR X .
+Proof.
+  intro X.
+  apply (quotientrep.R'_rep_τ_def congr_equivc compat_μ_projR (h:=hab)compat_rep_τ_projR isEpi_def_R'_rep_τ).
+Qed.
 
 Definition R'_rep : (rep_disp SET b).
   use tpair.
@@ -669,49 +545,27 @@ Open Scope arity_scope.
   
 (* Local Notation R'_REP := (R'_rep FepiR' aepiR). *)
 
-Lemma u_rep_laws : rep_ar_mor_law SET (R'_rep Fepi aepi) S (disp_nat_trans_id (pr1 b))
+(* TODO  : foutre ça dans quotientrep *)
+Let u_rep_laws : rep_ar_mor_law SET (R'_rep Fepi aepi) S (disp_nat_trans_id (pr1 b))
                                   (u_monad m).
 Proof.
   intro X.
-
-  (* but : utiliser R'_rep_τ_def *)
-  assert (epi : isEpi (* (C:=functor_Precategory SET SET) *)
-                  (   ((pr1 (# a (projR_monad))%ar)  ;;;
-                                                     (F``  R'_monad) )X    )).
-  {
-    
-    apply Pushouts_pw_epi.
-    apply PushoutsHSET_from_Colims.
-    apply isEpi_def_R'_rep_τ; assumption.
-  }
-  
-  apply epi.
-  
+  apply pathsinv0.
+  apply (
+               (quotientrep.u_rep_laws congr_equivc compat_μ_projR (h:=hab)compat_rep_τ_projR
+                                       (isEpi_def_R'_rep_τ Fepi aepi) (S:=pr1 S) (m:=pr1 m) _
+                                       (s:=rep_τ _ S) (F:=( (# b ( u_monad m))%ar))
+         )).
+  intro X'.
+  etrans; [apply (rep_ar_mor_ax _ m )|].
+  apply cancel_postcomposition.
   etrans.
-  apply assoc.
-  
+  (* # a m ;; F_S = #a π ;; F_R' ;; # b u *)
+  (* je dois utilier la naturalité de F à droite
+     pour avoir #a π ;; #a u ;; F_S et ensuite par définition de m = π ;; u
+   *)
+  apply cancel_postcomposition.
   etrans.
-  cpost _.
-  (*  apply (R'_rep_τ_def X) takes a long time, but use is immediate *)
-  use (R'_rep_τ_def Fepi aepi X).
-  
-  (* faire disparaitre le u avec u_def *)
-  etrans.
-  rewrite <- assoc.
-  cpre _.      
-  eapply pathsinv0.
-  apply (u_def m).
-  
-  (* utiliser le fait que m est un morphisme de representation *)
-  etrans.
-  apply (rep_ar_mor_ax _ m X).
-  
-  rewrite assoc.
-  cpost _.
-  
-  (* on réécrit m *)
-  etrans.
-  cpost _.
   apply (cancel_ar_on _ (compose (C:=category_Monad _) projR_monad (u_monad m))).
   use (invmap (Monad_Mor_equiv _ _ _)).
   { apply homset_property. }
@@ -719,47 +573,25 @@ Proof.
     apply homset_property.
     apply (u_def m).
   }
-  
-  etrans.
-  
-  etrans.
-  cpost _.
-  use (nat_trans_eq_pointwise _ X); cycle 1.
-  apply maponpaths.
   assert (yop:= @disp_functor_comp _ _ _ _ _ a ).
   assert (yop2 := fun xx yy zz  =>yop _ _ _ xx yy zz projR_monad (u_monad m)).
   assert (yop3 := yop2 (tt) (tt) (tt) (tt) (tt)).
+  unshelve eapply (nat_trans_eq_pointwise _ X').
+  eapply maponpaths in yop3.
   apply yop3.
-  (* todo : systématiser la séquence préécdente *)
-  cpost _.
-  cbn -[compose].
-  apply idpath.
-  
-  
-  etrans.
-  cpost _.
-  eapply (pathsinv0 (b:=pr1 (#a projR_monad)%ar X ;; pr1 (#a (u_monad m))%ar X )).
-  reflexivity.
-  etrans.
-  rewrite <- assoc.
-  cpre _.
-  etrans.
+  etrans;revgoals.
+  etrans;[|apply assoc].
+  apply cancel_precomposition.
   assert (hF :=disp_nat_trans_ax  (f:= (u_monad m)) (xx:=tt) (xx':=tt) F (tt)).
   apply LModule_Mor_equiv in hF.
-  eapply nat_trans_eq_pointwise in hF.
-  apply hF.
+  assert (hF':= (nat_trans_eq_pointwise hF X') ).
+  etrans;[apply hF'|].
+  clear.
+  set (e:=nat_trans_ax _ _ _ _ ).
+  unfold transportb.
+  now induction (!e).
   apply homset_property.
-  set (e:= nat_trans_ax _ _ _ _).
-  
-  unfold transportb.      
-  induction (!e).
-  
-  (* cbn -[compose]. *)
-  apply idpath.
-  apply funextfun.
-  intro x.
-  cbn.
-  apply idpath.
+  reflexivity.
 Qed.
 
 
