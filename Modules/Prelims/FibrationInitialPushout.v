c71805487c60262abd280d63988052bb60830f90
lafont <chaster_killer@hotmail.fr>
2018-04-13 21:26:31 +0200

    comments

--- Modules/Prelims/FibrationInitialPushout.v
+++ Modules/Prelims/FibrationInitialPushout.v
@@ -1,8 +1,37 @@
-(** Modularity in a fibration setting
+(** * Modularity in a fibration setting
+
+The main result of this file is described here.
+
+Let D be a fibration (ie, a cleaving in the displayed category setting)
+over a category C.
+
+ Suppose we have the following commuting diagram in the total category:
+
+<<<
+                  f1, ff1
+       c0 , d0 ------------>  c1 , d1
+          |                      |
+          |                      |
+f2 , ff2  |                      | g2 , gg2
+          |                      |
+          |                      |
+          |                      |
+          V                      V
+       c2 , d2 ------------>  c' , d'
+                 g1 , gg1
+>>>
+
+such that
+- the restricting diagram in the category C is a pushout
+- d1, d2, d' are initial in their respective fiber categories D[{c1}], D[{c2}], D[{d'}]
+   (this implies that gg1 and gg2 are uniquely determined : see [disp_InitialArrowUnique])
+
+Then the previous diagram is a pushout in the total category [pushout_total].
 
 
-
-
+As an auxiliary lemma, we show [disp_InitialArrowUnique] that if xx is initial
+in the fiber category over an object x of C, then there exists a unique displayed
+morphism over any C-morphism f with codomain x.
 
  *)
 Require Import UniMath.Foundations.PartD.
@@ -32,6 +61,10 @@ Section pr.
 
   Context   (cl : cleaving dC).
 
+  (**
+Let xx be initial in the fiber category over x ∈ C.
+If there is a morphism f : x → y  in C, then there is a unique morphism over f
+*)
   Definition disp_InitialArrow {x y : C} {xx : dC x} {yy : dC y}
         (init : isInitial  (fiber_category _ _) xx)
         (f : C ⟦ x, y ⟧) := 
@@ -58,63 +91,43 @@ Section pr.
       apply (maponpaths (fun x => (x;; _)%mor_disp)) .
       apply iscontr_uniqueness.
   Qed.
-  (*
-    revert ff ff'.
-    rewrite <- (id_left f).
-    intros ff ff'.
-    set (gg := cl _ _ f yy).
-    assert (hgg := cartesian_lift_is_cartesian _ _ gg).
-    etrans; [| use cartesian_factorisation_commutes; [ | |  exact hgg]].
-    assert (h : ff = (iscontrpr1 (init _) ;; gg)%mor_disp).
-    set (z := cl _ _ f yy).
-    etrans; [eapply pathsinv0; use cartesian_factorisation_commutes; [ | |  exact hgg]|].
-    apply (maponpaths (fun x => (x;; gg)%mor_disp)) .
-    apply proofirrelevancecontr.
-    apply init.
-  Qed.
-*)
+  
 
   Context
-
           {c0 c1 c2 c' : C}
           {f1 : C ⟦ c0, c1 ⟧}{f2 : C ⟦ c0, c2 ⟧}
           {g1 : C ⟦ c1, c' ⟧}{g2 : C ⟦ c2, c' ⟧}
-          {heq : f1 · g1 = f2 · g2}
-          (poC : isPushout f1 f2 g1 g2 heq).
+
+          {d0 :  dC c0} {d1 : dC c1} {d2 : dC c2} {d' : dC c'}
+          (ff1 : d0 -->[f1] d1)(ff2 : d0 -->[f2] d2)
+          (gg1 : d1 -->[g1] d')(gg2 : d2 -->[g2] d')
+
+          (init_d1 : isInitial  (fiber_category _ _) d1)
+          (init_d2 : isInitial  (fiber_category _ _) d2)
+          (init_d' : isInitial  (fiber_category _ _) d') 
+
+           (* Require commuting diagram in the total category *)
+          {eq_ff : TT ff1 · TT gg1 = TT ff2 · TT gg2}.
+
+  Let heq :  f1 · g1 = f2 · g2 := base_paths _ _ eq_ff.
+
+  Context (poC : isPushout f1 f2 g1 g2 heq).
 
   Let PO := mk_Pushout _ _ _ _ _ _ poC.
 
-  Context {d0 :  dC c0} {d1 : dC c1} {d2 : dC c2} {d' : dC c'}.
-  Context (ff1 : d0 -->[f1] d1)(ff2 : d0 -->[f2] d2)
-          (gg1 : d1 -->[g1] d')(gg2 : d2 -->[g2] d').
-
-  Context (init_d1 : isInitial  (fiber_category _ _) d1)
-          (init_d2 : isInitial  (fiber_category _ _) d2)
-          (init_d' : isInitial  (fiber_category _ _) d') .
-
-             
-
-  Context (eq_ff : TT ff1 · TT gg1 = TT ff2 · TT gg2).
 
   Lemma pushout_total : isPushout (TT ff1)(TT ff2)(TT gg1)(TT gg2) eq_ff.
   Proof.
     use mk_isPushout.
     intros [x xx] [h1 hh1] [h2 hh2] heq_hh.
     cbn in h1, hh1, h2, hh2.
-
-    assert (eq2 := fiber_paths  heq_hh).
-    set (eq1 := (base_paths _ _ heq_hh)) in eq2.
-
-    cbn in eq1,eq2.
     use unique_exists.
     - use TT.
       + apply (PushoutArrow PO _ h1 h2).
-        exact eq1.
-        (* exact (base_paths _ _ heq_hh). *)
+        exact (base_paths _ _ heq_hh).
       + apply disp_InitialArrow.
         assumption.
-    - cbn beta.
-      split.
+    - split.
       + use total2_paths2_b.
         * apply (PushoutArrow_PushoutIn1 PO).
         * etrans; [apply (disp_InitialArrowUnique init_d1)|].
@@ -137,4 +150,5 @@ Section pr.
          apply pathsinv0.
          apply (disp_InitialArrowUnique init_d').
   Qed.
+End pr.
 
