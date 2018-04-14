7e5f196e17b54c27c89fed19e1ab3fb491afb578
lafont <chaster_killer@hotmail.fr>
2018-04-14 14:31:21 +0200

    adapt modularity thm to the paper statment

--- Modules/Arities/Modularity.v
+++ Modules/Arities/Modularity.v
@@ -16,8 +16,9 @@ Suppose we have the following pushout diagram in the category of signatures:
                  g1 
 >>>
 
-such that a1, a2 and a' are representable and above this
-pushout there is a commuting square in the large category of representations:
+such that a0, a1, a2 and a' are representable with initial representations
+R0, R1, R2 and R'. Then, above this pushout there is a pushout square in the
+large category of representations:
 <<<
                 ff1
        R0 ------------>  R1 
@@ -31,10 +32,6 @@ pushout there is a commuting square in the large category of representations:
        R2 ------------>  R' 
                  gg1 
 >>>
-
-such that R1, R2, R' are the initial representations of respectively a1, a2 and a'.
-
-Then the previous diagram is a pushout in the large category of representations [pushout_in_big_rep].
 
 
  *)
@@ -56,7 +53,8 @@ Require Import UniMath.CategoryTheory.DisplayedCats.Fibrations.
 Require Import Modules.Arities.aritiesalt.
 Require Import Modules.Prelims.FibrationInitialPushout.
 
-Section pr.
+
+Section init.
   Context {C : category} (dC : disp_cat C).
 
   Local Notation TT := (disp_mor_to_total_mor (rep_disp C)).
@@ -66,25 +64,32 @@ Section pr.
           {g1 : arity_category ⟦ a1, a' ⟧}{g2 : arity_category ⟦ a2, a' ⟧}
 
           {R0 : rep_disp _  a0} {R1 : rep_disp _  a1} {R2 : rep_disp _  a2} {R' : rep_disp _ a'}
-          (ff1 : R0 -->[f1] R1)(ff2 : R0 -->[f2] R2)
-          (gg1 : R1 -->[g1] R')(gg2 : R2 -->[g2] R')
-
+          (repr_R0 : isInitial  (fiber_category _ _) R0)  
           (repr_R1 : isInitial  (fiber_category _ _) R1)
           (repr_R2 : isInitial  (fiber_category _ _) R2)
           (repr_R' : isInitial  (fiber_category _ _) R') .
 
-           (** Require commuting diagram in the big representation category *)
-  Context {eq_ff : TT ff1 · TT gg1 = TT ff2 · TT gg2}.
+  Context  (heq :  f1 · g1 = f2 · g2)
+    (poC : isPushout f1 f2 g1 g2 heq).
 
-  Let heq :  f1 · g1 = f2 · g2 := base_paths _ _ eq_ff.
 
-  Context (poC : isPushout f1 f2 g1 g2 heq).
+  Let ff1 : R0 -->[f1] R1 :=  disp_InitialArrow (rep_disp C) (rep_cleaving C) repr_R0 f1.
+  Let ff2 : R0 -->[f2] R2 :=  disp_InitialArrow (rep_disp C) (rep_cleaving C) repr_R0 f2.
+  Let gg1 : R1 -->[g1] R' :=  disp_InitialArrow (rep_disp C) (rep_cleaving C) repr_R1 g1.
+  Let gg2 : R2 -->[g2] R' :=  disp_InitialArrow (rep_disp C) (rep_cleaving C) repr_R2 g2.
 
-  Let PO := mk_Pushout _ _ _ _ _ _ poC.
+  Let eq_ff : TT ff1 · TT gg1 = TT ff2 · TT gg2.
+  Proof.
+    use total2_paths2_b.
+    - exact heq.
+    - etrans; [ apply (disp_InitialArrowUnique _ (rep_cleaving C) repr_R0) | ].
+      apply pathsinv0.
+      apply (disp_InitialArrowUnique _ (rep_cleaving C) repr_R0).
+  Qed.
 
 
   Definition pushout_in_big_rep : isPushout (TT ff1)(TT ff2)(TT gg1)(TT gg2) eq_ff :=
     pushout_total (rep_disp C) (rep_cleaving C) ff1 ff2 gg1 gg2 repr_R1 repr_R2 repr_R' poC.
 
 
-End pr.
+End init.
