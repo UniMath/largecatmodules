3bdaf0bcee629d3d8135addd241064e0fd6e7cb3
Ambroise <chaster_killer@hotmail.fr>
2018-10-04 18:10:49 +0200

    Faithful Opfibrations lifts  coequalizers

--- Modules/Prelims/FaithfulFibrationEqualizer.v
+++ Modules/Prelims/FaithfulFibrationEqualizer.v
@@ -7,6 +7,8 @@ over a category C such that the projection functor is faithful.
 
 Then the projection functor lifts equalizers.
 
+We also prove the dual statement (opfibration and coequalizers)
+
  *)
 Require Import UniMath.Foundations.PartD.
 Require Import UniMath.Foundations.Propositions.
@@ -25,6 +27,9 @@ Require Import UniMath.CategoryTheory.DisplayedCats.Fibrations.
 Require Import UniMath.CategoryTheory.limits.graphs.colimits.
 Require Import UniMath.CategoryTheory.limits.graphs.limits.
 Require Import UniMath.CategoryTheory.limits.graphs.equalizers.
+Require Import UniMath.CategoryTheory.limits.graphs.coequalizers.
+
+Require Import Modules.Prelims.Opfibration.
 
 Section pr.
   Context {C : category} (D : disp_cat C).
@@ -92,3 +97,65 @@ This can be reformulated as [faithful pr1_category) as in [faithful_pr1_category
   Defined.
 End pr.
 
+
+Section oppr.
+  Context {C : category} (D : disp_cat C).
+
+
+  (** This is a opfibration *)
+  Context   (cl : opcleaving D).
+  (** The opfibration is faithful
+This can be reformulated as [faithful pr1_category) as in [faithful_pr1_category]
+   *)
+  Hypothesis (faithful_opfibration : faithful (pr1_category D)).
+
+  Local Notation faithful_reformulated := (faithful_reformulated  D  faithful_opfibration).
+
+  Lemma faithful_opfibration_coequalizer {a b} (f g : total_category D⟦a, b⟧)
+        ( eq  : Coequalizer _ (# (pr1_category D) f)%cat (# (pr1_category D) g))
+        : Coequalizer _ f g.
+  Proof.
+    use mk_Coequalizer.
+    - refine (CoequalizerObject _ eq ,, _).
+      eapply (opcleaving_ob cl ).
+      + apply CoequalizerArrow.
+      + exact (pr2 b).
+    - refine (CoequalizerArrow _ _ ,, _).
+      apply opcleaving_mor.
+    - apply faithful_reformulated.
+      cbn.
+      apply CoequalizerArrowEq.
+    - use mk_isCoequalizer.
+      + apply homset_property.
+      + intros e h' eqh'.
+        assert (h := CoequalizerArrowComm _ eq _ _ (base_paths _ _ eqh')).
+        set (ar_base := CoequalizerOut  _ eq _ _ (base_paths _ _ eqh')) in h.
+        use unique_exists.
+        * refine (ar_base ,, _).
+          cbn.
+          eapply cocartesian_factorisation.
+          -- use cocartesian_lift_is_cocartesian.
+          -- generalize (pr2 h').
+             apply transportb.
+             exact h.
+        * cbn.
+          apply subtypePairEquality'.
+          --  exact h.
+          --  apply invproofirrelevance.
+              intros u v.
+              apply pair_inj.
+              ++ apply homset_property.
+              ++ apply faithful_reformulated.
+                 apply idpath.
+        * cbn -[isaprop].
+          intro y.
+          apply (homset_property (total_category D)).
+        * 
+          intros h'2 eqh'2.
+          apply faithful_reformulated.
+          cbn.
+          apply CoequalizerOutUnique.
+          apply base_paths in  eqh'2.
+          exact eqh'2.
+  Defined.
+End oppr.
