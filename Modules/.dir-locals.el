bf2e3e4bac441f2ef5f8c3b4e7cbc0e80101aec0
Ralph Matthes <rmatthes@users.noreply.github.com>
2024-01-25 19:55:55 +0100

    corrects an unwanted commit

--- Modules/.dir-locals.el
+++ Modules/.dir-locals.el
@@ -2,5 +2,4 @@
   . ((eval . 
 	   (progn
 	     (make-local-variable 'coq-prog-args)
-	     (setq coq-prog-args `("-emacs" "-noinit" "-indices-matter" "-type-in-type"
-"-R" "/home/matthes/Documents/Work/Research/Projects/UniMath/Githubbereich/UniMath/_build/default/UniMath" "UniMath" "-R"  "/home/matthes/Documents/Work/Research/Projects/UniMath/Githubbereich/UniMath/_build/default/largecatmodules" "largecatmodules" )))))))
+	     (setq coq-prog-args `("-emacs" "-noinit" "-indices-matter" "-type-in-type" "-R" ,(expand-file-name (locate-dominating-file buffer-file-name ".dir-locals.el")) "Modules" )))))))
